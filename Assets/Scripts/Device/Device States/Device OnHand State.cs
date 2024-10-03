5a47b70a631a13da385b9b0ae3c8c1b834fb93bb
mleinad <101401882+mleinad@users.noreply.github.com>
2024-10-03 22:32:01 +0100

    improved map script (state machine)

---
+++ Assets/Scripts/Device/Device States/Device OnHand State.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeviceOnHandState : DeviceBaseState
+{
+    string trigger = "position1";
+   public override void EnterState(DeviceStateManager context){
+    context.animator.SetTrigger(trigger);
+
+    }
+
+   public override void UpdateState(DeviceStateManager context){
+        
+        if(Input.GetKeyDown(KeyCode.Tab)){
+            context.SwitchState(context.deviceFullScreen_state);
+        }
+        if(Input.GetKeyDown(KeyCode.Q)){
+            context.SwitchState(context.deviceStored_state);
+        }
+    }
+
+   public override void ExitState(DeviceStateManager context){
+         Debug.Log("exit hand");
+
+    }
+}
+
