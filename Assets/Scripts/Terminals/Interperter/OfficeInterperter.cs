618420ca5187526a61271237a93e3e7314ab1709
GabrielCosta123 <megapidgey.69@gmail.com>
2024-11-18 14:31:38 +0000

    Password Puzzle update.

--- Assets/Scripts/Terminals/Interperter/OfficeInterperter.cs
+++ Assets/Scripts/Terminals/Interperter/OfficeInterperter.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class OfficeInterperter : MonoBehaviour, Iinterperter
 {
-
     List<string> response = new List<string>();
+    private string correctPassword = "earth"; // Palavra-passe correta
+
     public List<string> Interpert(string input)
     {
         response.Clear();
@@ -17,9 +18,17 @@ public class OfficeInterperter : MonoBehaviour, Iinterperter
             response.Add("help");
             return response;
         }
-        if (args[0] == "example")
+        if (args[0] == "password") 
         {
-            response.Add("....");
+            if (args.Length > 1 && args[1] == correctPassword)
+            {
+                OpenDoor(); 
+                response.Add("Access granted. Opening Door.");
+            }
+            else
+            {
+                response.Add("Incorrect password.");
+            }
             return response;
         }
         else
@@ -27,5 +36,11 @@ public class OfficeInterperter : MonoBehaviour, Iinterperter
             response.Add("command not recognized");
             return response;
         }
+    }
+
+    private void OpenDoor()
+    {
+        // Realize a ação desejada aqui
+        Debug.Log("Password is correct! Opening Door...");
     }
 }
