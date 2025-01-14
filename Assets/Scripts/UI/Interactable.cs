c0a46eaf36e2cb2b212805f4ed7c87223f997e37
Rafael Enes <a25980@alunos.ipca.pt>
2025-01-14 15:54:47 +0000

    CutSceneFile

--- Assets/Scripts/UI/Interactable.cs
+++ Assets/Scripts/UI/Interactable.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using JetBrains.Annotations;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
 using UnityEngine.UI;
@@ -11,18 +12,19 @@ public class Interactable : MonoBehaviour
 {
     private static readonly int Triggered = Animator.StringToHash("triggered");
     private static readonly int Interaction = Animator.StringToHash("interaction");
-    
+
     bool interactable = false;
     bool isOn;
     bool triggered;
-    
+
     public bool repeat;
     bool beenTriggerd;
 
     private InteractableAction interactableAction;
     private Terminal terminal;
     private AudioSource audioSource;
-    void Awake(){
+    void Awake()
+    {
         isOn = false;
         triggered = false;
 
@@ -35,17 +37,21 @@ public class Interactable : MonoBehaviour
             audioSource.playOnAwake = false; // Prevent audio from playing immediately
         }
     }
-    void OnTriggerEnter(Collider other){
-        
-        if(other.GetComponent<CharacterController>()){
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.GetComponent<CharacterController>())
+        {
             interactable = true;
             isOn = true;
         }
     }
 
-    
-    void OnTriggerExit(Collider other){
-        if(other.GetComponent<CharacterController>()){
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<CharacterController>())
+        {
             interactable = false;
             isOn = true;
             triggered = false;
@@ -56,31 +62,31 @@ public class Interactable : MonoBehaviour
 
     void Update()
     {
-            if(!repeat)
+        if (!repeat)
+        {
+            if (triggered)
             {
-                if(triggered)
-                {
-                    return; //instead of this.enabled = false
-                }
+                return; //instead of this.enabled = false
             }
-            if (!GetComponent<Collider>().enabled)
+        }
+        if (!GetComponent<Collider>().enabled)
+        {
+            isOn = false;
+        }
+
+        if (interactable)
+        {
+
+            if (Input.GetKeyDown(KeyCode.E))
             {
-                isOn = false;
+                beenTriggerd = !beenTriggerd;
+                Action();
+                StartCoroutine(OffDelay());
+                triggered = true;
             }
 
-            if(interactable)
-            {
-
-                if(Input.GetKeyDown(KeyCode.E))
-                {   
-                    beenTriggerd = !beenTriggerd;
-                    Action();
-                    StartCoroutine(OffDelay());
-                    triggered = true;
-                }
-
-                Player.Instance.MessageOn(isOn);
-            }
+            Player.Instance.MessageOn(isOn);
+        }
         PlaySound();
     }
 
@@ -90,73 +96,100 @@ public class Interactable : MonoBehaviour
         yield return new WaitForSeconds(1f);
         isOn = true;
     }
-    public bool WasTriggered()=> triggered;
+    public bool WasTriggered() => triggered;
 
     public void SetTrigger(bool b) => triggered = b;
 
     void Action()
     {
-        EventManager.Instance.OnTriggerInteraction(this);   
+        EventManager.Instance.OnTriggerInteraction(this);
     }
     private void PlaySound()
     {
+         bool soundPlayed = false;
+
         if (audioSource == null) return;
+
+        audioSource.loop = false;
 
         if (interactableAction != null)
         {
-            // Determine the sound based on the animator controller
             var animator = interactableAction.GetComponent<Animator>();
             if (animator != null)
             {
                 var runtimeAnimatorController = animator.runtimeAnimatorController;
                 if (runtimeAnimatorController != null)
                 {
-                    if (runtimeAnimatorController.name == "SM_Bld_Door_Single_03 (1)")
+                    AudioClip clipToPlay = null;
+
+                    // Check the runtimeAnimatorController name
+                    switch (runtimeAnimatorController.name)
                     {
-                        if (interactableAction.beenTriggerd)
-                        {
-                            audioSource.clip = Resources.Load<AudioClip>("SoundDoor1");
-                        }
-                        
+                        case "SM_Bld_Door_Single_03 (1)":
+                            if (interactableAction.beenTriggerd && !soundPlayed)
+                            {
+                                clipToPlay = Resources.Load<AudioClip>("Door1");
+                                soundPlayed = true; 
+                            }
+                            else if (!interactableAction.beenTriggerd && soundPlayed)
+                            {
+                                clipToPlay = Resources.Load<AudioClip>("Door1");
+                                soundPlayed = false; 
+                            }
+                            break;
+
+                        case "door3":
+                            if (interactableAction.beenTriggerd && !soundPlayed)
+                            {
+                                clipToPlay = Resources.Load<AudioClip>("Door2");
+                                soundPlayed = true;
+                            }
+                            else if (!interactableAction.beenTriggerd && soundPlayed)
+                            {
+                                clipToPlay = Resources.Load<AudioClip>("Door2");
+                                soundPlayed = false;
+                            }
+                            break;
+
+                        case "Locker":
+                            
+                            if (animator.GetBool("triggered"))
+                            {
+                                clipToPlay = Resources.Load<AudioClip>("SoundLocker");
+                                
+                            }
+                            break;
                     }
-                    else if (runtimeAnimatorController.name == "door3")
+
+                    // Play the determined clip
+                    if (clipToPlay != null && (!audioSource.isPlaying || audioSource.clip != clipToPlay))
                     {
-                        if (interactableAction.beenTriggerd) 
-                        {
-                            audioSource.clip = Resources.Load<AudioClip>("SoundDoor2");
-                        } 
-                        
+                        audioSource.clip = clipToPlay;
+                        audioSource.Play();
                     }
-                    else if(runtimeAnimatorController.name =="Locker")
-                    {
-                        if(animator.GetBool("interaction") == true)
-                        {
-                            audioSource.clip = Resources.Load<AudioClip>("SoundLocker");
-                        }else if(animator.GetBool("interaction") == false)
-                        {
-                            audioSource.clip = Resources.Load<AudioClip>("SoundLockerClose");
-                        }    
-                        
-                    }
-                    audioSource.Play();
+                  
                 }
             }
         }
         else if (terminal != null)
         {
-            // Play terminal interaction sound
             if (WasTriggered())
             {
-                audioSource.clip = Resources.Load<AudioClip>("TerminalSound");
-                if (audioSource.clip == null)
+                AudioClip terminalClip = Resources.Load<AudioClip>("TerminalSound");
+
+                if (terminalClip != null)
                 {
-                    Debug.LogError("Failed to load SoundOne. Check the file name and ensure it's in the Resources folder.");
+                    if (!audioSource.isPlaying || audioSource.clip != terminalClip)
+                    {
+                        audioSource.clip = terminalClip;
+                        audioSource.Play();
+                    }
                 }
-                audioSource.Play();
+                else
+                {
+                    Debug.LogError("Failed to load TerminalSound. Ensure the file is in the Resources folder.");
+                }
             }
-        }
-        else
-        {
         }
     }
 }
